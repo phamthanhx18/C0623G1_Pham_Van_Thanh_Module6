e6b203100dd5ca18a2ac071acc095bd5e8fa8a22
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-12-26 08:19:04 +0700

    up

--- backend-project/fashion-shop/src/main/java/com/example/fashionshop/config/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.example.fashionshop.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
